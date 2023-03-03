fada22f55f5944071ec227a23ee31f24b880c416
Felipe <felipesantosdd@gmail.com>
2023-03-03 17:01:59 -0300

    feat: Ready to Production.

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,20 +1,65 @@
 import 'reflect-metadata'
 import 'dotenv/config'
 import path from 'path'
-import { DataSource } from 'typeorm'
+import { DataSource, DataSourceOptions } from 'typeorm'
 
-const AppDataSource = new DataSource({
-    type: 'postgres',
-    host: process.env.DB_HOST,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    port: parseInt(process.env.DB_PORT),
-    database: process.env.DB,
-    synchronize: false,
-    logging: true,
-    entities: [path.join(__dirname, './entities/**.{js,ts}')],
-    migrations: [path.join(__dirname, './migrations/**.{js,ts}')]
-})
+const setDataSourceConfig = (): DataSourceOptions => {
+    const entitiesPath: string = path.join(__dirname, "./entities/**.{js,ts}");
+    const migrationsPath: string = path.join(
+        __dirname,
+        "./migrations/**.{js,ts}"
+    );
 
-export default AppDataSource
+    const nodeEnv = process.env.NODE_ENV;
+
+    if (nodeEnv === "production") {
+        return {
+            type: "postgres",
+            url: process.env.DATABASE_URL,
+            entities: [entitiesPath],
+            migrations: [migrationsPath],
+        };
+    }
+
+    if (nodeEnv === "test") {
+        return {
+            type: "sqlite",
+            database: ":memory:",
+            synchronize: true,
+            entities: [entitiesPath],
+        };
+    }
+
+    return {
+        type: "postgres",
+        host: process.env.PGHOST,
+        username: process.env.PGUSER,
+        password: process.env.PGPASSWORD,
+        port: parseInt(process.env.PGPORT),
+        database: process.env.DB,
+        synchronize: false,
+        logging: true,
+        entities: [entitiesPath],
+        migrations: [migrationsPath],
+    };
+};
+
+const dataSourceConfig = setDataSourceConfig();
+export default new DataSource(dataSourceConfig);
+
+
+// const AppDataSource = new DataSource({
+//     type: 'postgres',
+//     host: process.env.DB_HOST,
+//     username: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     port: parseInt(process.env.DB_PORT),
+//     database: process.env.DB,
+//     synchronize: false,
+//     logging: true,
+//     entities: [path.join(__dirname, './entities/**.{js,ts}')],
+//     migrations: [path.join(__dirname, './migrations/**.{js,ts}')]
+// })
+
+// export default AppDataSource
 
---
+++ src/migrations/1677873679924-forProduction.ts
@@ -0,0 +1,40 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class forProduction1677873679924 implements MigrationInterface {
+    name = 'forProduction1677873679924'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE "cash_registers" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "description" character varying(100) NOT NULL, "date" TIMESTAMP NOT NULL, "value" double precision NOT NULL, "type" character varying(50) NOT NULL, "payment" character varying(50) NOT NULL, "user_id" uuid NOT NULL, CONSTRAINT "PK_c1cc711056395d079d8f041ce34" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "name"`);
+        await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "age"`);
+        await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "cpf"`);
+        await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "isActive"`);
+        await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "createdAt"`);
+        await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "updatedAt"`);
+        await queryRunner.query(`ALTER TABLE "users" ADD "userName" character varying(100) NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "users" DROP CONSTRAINT "UQ_97672ac88f789774dd47f7c8be3"`);
+        await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "email"`);
+        await queryRunner.query(`ALTER TABLE "users" ADD "email" character varying(100) NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "password"`);
+        await queryRunner.query(`ALTER TABLE "users" ADD "password" character varying(120) NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "cash_registers" ADD CONSTRAINT "FK_f860498b8094442a69ca06f6ee2" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "cash_registers" DROP CONSTRAINT "FK_f860498b8094442a69ca06f6ee2"`);
+        await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "password"`);
+        await queryRunner.query(`ALTER TABLE "users" ADD "password" character varying NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "email"`);
+        await queryRunner.query(`ALTER TABLE "users" ADD "email" character varying NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "users" ADD CONSTRAINT "UQ_97672ac88f789774dd47f7c8be3" UNIQUE ("email")`);
+        await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "userName"`);
+        await queryRunner.query(`ALTER TABLE "users" ADD "updatedAt" TIMESTAMP NOT NULL DEFAULT now()`);
+        await queryRunner.query(`ALTER TABLE "users" ADD "createdAt" TIMESTAMP NOT NULL DEFAULT now()`);
+        await queryRunner.query(`ALTER TABLE "users" ADD "isActive" boolean NOT NULL DEFAULT true`);
+        await queryRunner.query(`ALTER TABLE "users" ADD "cpf" character varying NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "users" ADD "age" integer NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "users" ADD "name" character varying NOT NULL`);
+        await queryRunner.query(`DROP TABLE "cash_registers"`);
+    }
+
+}
